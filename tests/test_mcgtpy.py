7e5b5af4b1b7ffaf63191c989d40f9f7fb60eea7
MylesThomas <mylescgthomas@gmail.com>
2023-05-05 10:48:14 -0500

    Adding a 2nd venv, as well as the tests folder with testing configuration

---
+++ tests/test_mcgtpy.py
@@ -0,0 +1,25 @@
+# Import methods from our Modules
+from mcgtpy.my_stats import print_stats
+from mcgtpy.helper_functions import columns_janitor
+
+# Print stats (we don't need this necessarily ...)
+print_stats()
+
+# Tests to make sure columns_janitor() is on point
+def test_1():
+    input = ['  MYleS  __    _', '  _ chRistian______ George___', '____ tHomas ']
+    actual = columns_janitor(input)
+    expected = ['myles', 'christian_george', 'thomas']
+    assert actual == expected
+
+def test_2():
+    input = ['Hey', "Hello"]
+    actual = columns_janitor(input)
+    expected = ["hey", "hello"]
+    assert actual == expected
+
+def test_3():
+    input = ['HEY    __  ', "     A   ___  b ___ C "]
+    actual = columns_janitor(input)
+    expected = ["hey", "a_b_c"]
+    assert actual == expected
